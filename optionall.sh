eb198b13e112f8aaab3f4f58ce71b0b5dba0bf5a
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2025-03-23 22:27:01 +0100

    WIP implement callback style

--- optionall.sh
+++ optionall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dbg() { printf '%s\n' "$*" >&2; }
+dbg() { printf '{%s} ' "$@" >&2; printf '\n'; }
 
 printf 'ARGS:'
 printf ' [%s]' "$@"
@@ -8,46 +8,134 @@ printf '\n'
 
 # TODO tests
 # TODO don't parse "-N123" as "-123" if "-N" is unknown?
+# TODO dynamic code gen to be eval'd?
+# TODO static code gen?
+# TODO let users handle end of options via callback?
 
-parseopts() {
-	arg=${arg#-}
-	while [ "$arg" ]; do
-		value=${arg#?} a=${arg%"$value"} opt_state=
-		value=${value:-$opt}
-		case "$a" in
-			v) verbosity=$((verbosity+1)) ;;
-			h) help=1 ;;
-			n) number=$value; break ;;
-			d) delim=$value; break ;;
-			f) field=$value; break ;;
-			[0-9]*) number=$arg; break ;;
-			# *) echo "Unknown option $a!" >&2; exit 1; break ;; # TODO
-		esac
-		arg=$value
-	done
+is_function() {
+	case "$(type -- "$1" 2>/dev/null)" in
+		*function*) return 0 ;;
+		*) return 1 ;;
+	esac
 }
 
-parsevalue() { [ "$value" = "$arg" ] && value=$opt opt_state=skip; }
+if ! is_function argument; then
+argument() {
+	arg=$1 value=$2 orig=$3
+	dbg argument "arg=$arg" "val=$value" "orig=$orig"
+	case "$arg" in
+		--name) name=$value; return 1 ;;
+		--age) age=$value; return 1 ;;
+		--help|-h) help=1; return 0 ;;
+		-v) verbosity=$((verbosity+1)); return 0 ;;
+		-d) delim=$value; return 1 ;;
+		-f) field=$value; return 1 ;;
+		-n) number=$value; return 1 ;;
+		-[0-9]*) number=${orig#-}; return 0 ;;
+		-?) echo "Invalid option $arg"; exit 1 ;;
+		--*) echo "Invalid option $arg"; exit 1 ;;
+		# *) return 1 ;;
+	esac
+}
+fi
+
+parselong() {
+	dbg parselong
+	value=${arg#*=}
+	if [ "$value" = "$arg" ]; then
+		value=$next opt_state=skip
+	fi
+	argument "${arg%%=*}" "$value" "$arg"
+	case "$?" in
+		0) opt_state= ;;
+		*) ;;
+	esac
+}
+
+parseshort() {
+	dbg parseshort
+	fullarg=$arg
+	arg=${arg#-}
+	while [ "$arg" ]; do
+		value=${arg#?}
+		set -- "$value"
+		a=-${arg%"$value"}
+		if ! [ "$value" ]; then
+			dbg emptyvalue
+			value=$next opt_state=skip
+		fi
+		argument "$a" "$value" "$fullarg"
+		case "$?" in
+			0) opt_state= ;;
+			*) break ;;
+		esac
+		arg=$1
+	done
+}
 
 opt_state=
 for arg; do
 	shift
-	[ "$opt_state" = end ] && { set -- "$@" "$arg"; continue; }
+	dbg "handling" "$arg"
 
-	value=${arg#*=} opt=$1
+	case "$opt_state" in
+		end) set -- "$@" "$arg"; continue ;;
+		skip) opt_state=; continue ;;
+	esac
+
+	next=$1
 	case "$arg" in
 		# end of options
 		--) opt_state=end ;;
-		# long args
-		--help) help=1 ;;
-		# long args with values
-		--name*) parsevalue; name=$value ;;
-		--age*) parsevalue; age=$value ;;
+		# long args (with values)
+		--?*) parselong ;;
 		# short args
-		-?*) parseopts ;;
-		*) [ "$opt_state" = skip ] || set -- "$@" "$arg"; opt_state= ;;
+		-?*) parseshort ;;
+		# non-args
+		*) set -- "$@" "$arg" ;;
 	esac
 done
+
+optionall_parse() {
+	parsed=$1
+	unparsed=${2:-set}
+	shift 2
+
+	optionall_init
+	for arg; do
+		shift
+		# [ "$opt_state" = end ] && { set -- "$@" "$arg"; continue; }
+		if [ "$opt_state" = end ]; then
+			set -- "$@" "$arg";
+			"$unparsed" "$arg";
+			continue;
+		fi
+		if [ "$opt_state" = skip ]; then
+			dbg "skipping $arg"
+			opt_state=
+			continue
+		fi
+
+		value=${arg#*=} next=$1
+		case "$arg" in
+			# end of options
+			--) opt_state=end ;;
+			# long args (with values)
+			--?*) parselong "$@" ;;
+			# short args
+			-?*) parseshort "$@" ;;
+			# non-args
+			# *) set -- "$@" "$arg"; opt_state= ;;
+			# TODO which parameters to pass to unparsed? allow to pass "set"?
+			# TODO implement special handling for "set"?
+			# TODO how to handle "end of options" ('--')?
+			# *) "$unparsed" -- "$@" "$arg"; opt_state= ;;
+			*)
+				set -- "$@" "$arg"; opt_state=
+				"$unparsed" "$arg" ;;
+		esac
+	done
+}
 
 printf 'PARSED:'
 printf ' [%s]' "$@"
@@ -59,4 +147,5 @@ echo "age=$age"
 echo "verbosity=$verbosity"
 echo "number=$number"
 echo "delimiter=$delim"
+echo "delimiter='$delim'"
 echo "field=$field"
